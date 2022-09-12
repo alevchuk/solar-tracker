fc8542b3fc3b536b488ddf49ac2de9979b09ce7d
alevchuk <alevchuk@example.com>
2022-09-12 07:16:23 -0700

    more reproducibility docs for plot1_timeseries.py

--- analysis/README.md
+++ analysis/README.md
@@ -6,6 +6,18 @@ run:
 . ./bin/activate
 ```
 
+== (sometimes) Every time you get a new measurements.txt file ==
+```
+echo ts,delay,gen,ext,count,angle > ~/measurements.csv
+cat ~/measurements.txt | tr '\t' ',' >> ~/measurements.csv
+```
+
+== (rare) Every time you get a fresh linux install ==
+```
+sudo apt-get install python3-tk
+sudo apt-get install python3-virtualenv
+```
+
 == (rare) Every time you clone github repo ==
 ```
 cd solar-tracker
--- analysis/plot1_timeseries.py
+++ analysis/plot1_timeseries.py
@@ -6,20 +6,26 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 
-ext = True
 series = read_csv('~/measurements.csv', header=0, index_col=4)
 
-plot1 = series[series["gen"] == 1][series["ext"] == ext].plot(y="angle", fontsize=20)
+ext = False
+gen = 5247
+series = series[series["gen"] == gen]
+series = series[series["ext"] == ext]
+plot1 = series.plot(y="angle", fontsize=20)
 plot1.set_title("Step size: 1 second", fontsize=50)
 
-plot2 = series[series["gen"] == 10][series["ext"] == ext].plot(y="angle", fontsize=20)
-plot2.set_title("Step size: 500 milliseconds", fontsize=50)
+#plot1 = series[series["gen"] == gen][series["ext"] == ext].plot(y="angle", fontsize=20)
+#plot1.set_title("Step size: 1 second", fontsize=50)
 
-plot2 = series[series["gen"] == 20][series["ext"] == ext].plot(y="angle", fontsize=20)
-plot2.set_title("Step size: 100 milliseconds", fontsize=50)
-
-plot2 = series[series["gen"] == 30][series["ext"] == ext].plot(y="angle", fontsize=20)
-plot2.set_title("Step size: 50 milliseconds", fontsize=50)
+#plot2 = series[series["gen"] == 10][series["ext"] == ext].plot(y="angle", fontsize=20)
+#plot2.set_title("Step size: 500 milliseconds", fontsize=50)
+#
+#plot2 = series[series["gen"] == 20][series["ext"] == ext].plot(y="angle", fontsize=20)
+#plot2.set_title("Step size: 100 milliseconds", fontsize=50)
+#
+#plot2 = series[series["gen"] == 30][series["ext"] == ext].plot(y="angle", fontsize=20)
+#plot2.set_title("Step size: 50 milliseconds", fontsize=50)
 
 #plot2 = series[series["gen"] == 98][series["ext"] == ext].plot(y="angle", fontsize=20)
 #plot2.set_title("Step size: 20 milliseconds", fontsize=50)
